32f74906ab3b34d2feaa138d1f0090fff7749fc1
05-archana <144509088+05-archana@users.noreply.github.com>
2024-04-12 10:49:22 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * Hello world!
+ *
+ */
+public class addition
+{
+    public static void main( String[] args )
+    {
+        int a=45,b=20,sum;
+        sum=a+b;
+        System.out.println("sum of the numbers : "+sum);
+    }
+}
